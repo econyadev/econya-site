82400d04bcd990949c87a1d91066dd2b52882478
econyadev <omichessanh@gmail.com>
2025-08-14 23:13:19 +0200

    Update env.js

--- assets/env.js
+++ assets/env.js
@@ -1,1 +1,4 @@
-window.ECONYA_API_BASE = "https://econya-backend.onrender.com";
+// assets/env.js
+window.ECONYA_API_BASE = "https://econya-backend.onrender.com";  // ne pas modifier ni commenter
+window.ECONYA_PLAN = "gratuit";  // ou "premium"
+
