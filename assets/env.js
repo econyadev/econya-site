db379221c8eb9b7b1c9361f654dd95a7cc4c7e4b
econyadev <omichessanh@gmail.com>
2025-08-14 21:59:51 +0200

    Update env.js

--- assets/env.js
+++ assets/env.js
@@ -1,2 +1,4 @@
 // assets/env.js
 window.ECONYA_API_BASE = "https://econya-backend.onrender.com";
+<!-- charge-le APRES assets/env.js dans toutes les pages -->
+<script src="actifs/main-pages.js"></script>
